dfa11b15f501f95188dcf2b1f6d5713c91930374
aya <56827395+almayra@users.noreply.github.com>
2021-01-18 15:28:20 +0700

    [UI]: add pagination UI

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ChevronLeft16, ChevronRight16 } from "@carbon/icons-react";
+import "./pagination.scss";
+
+export const Pagination = () => {
+  return (
+    <ul className="pagination">
+      <li className="arrow">
+        <button>
+          <ChevronLeft16 />
+        </button>
+      </li>
+      <li className="number">
+        <button>1</button>
+      </li>
+      <li className="number">
+        <button>2</button>
+      </li>
+      <li className="number">
+        <button>3</button>
+      </li>
+      <li className="number">
+        <button>4</button>
+      </li>
+      <li className="number active">
+        <button>5</button>
+      </li>
+      <li className="arrow">
+        <button>
+          <ChevronRight16 />
+        </button>
+      </li>
+    </ul>
+  );
+};
---
+++ src/components/Pagination/pagination.stories.js
@@ -0,0 +1,12 @@
+import { Pagination } from "./Pagination";
+
+export const FivePagePaginaton = () => {
+  return <Pagination />;
+};
+
+const PaginationStory = {
+  title: "Molecule/Pagination",
+  component: Pagination,
+};
+
+export default PaginationStory;
