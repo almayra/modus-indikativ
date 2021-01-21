d005367bb71acdc47cc93142c6cb13127898bb6a
aya <56827395+almayra@users.noreply.github.com>
2021-01-21 16:28:13 +0700

    [UI]: add chat room

---
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import "./chatroom.scss";
+import ChatBottomText from "./ChatBottomText";
+import BodyShort from "../Typography/BodyShort";
+
+interface IChatRoom {
+  sender: "outbound-chat" | "inbound-chat";
+  name: string;
+  time: string;
+  messageText: string;
+  prepend?: ReactNode;
+  append?: ReactNode;
+  img?: ReactNode;
+  linkText?: boolean;
+}
+
+export const ChatRoom = ({
+  name,
+  time,
+  messageText,
+  prepend,
+  append,
+  img,
+  linkText,
+  sender = "inbound-chat",
+}: IChatRoom) => {
+  return (
+    <div className={`chat-room ${sender}`}>
+      <div className="bubble">
+        {img ? (
+          <div className={` ${img ? "with-image" : ""}`}>{img}</div>
+        ) : null}
+        <div className="text-message">
+          {prepend ? <span className="icon prepend">{prepend} </span> : ""}
+          <BodyShort className={`text-message ${linkText ? "link" : ""}`}>
+            {messageText}
+          </BodyShort>
+          {append ? <span className="icon append">{append}</span> : ""}
+        </div>
+      </div>
+      <ChatBottomText className="bottom-text">
+        {name} • {time}
+      </ChatBottomText>
+    </div>
+  );
+};
