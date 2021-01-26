733cb7a60f71f36a8e9117b35c854a4c1f3244b1
aya <56827395+almayra@users.noreply.github.com>
2021-01-26 16:27:49 +0700

    [UI]: changes in parent list

--- src/components/ParentList/ParentList.tsx
+++ src/components/ParentList/ParentList.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./parentlist.scss";
 import { Settings24, ChevronDown24 } from "@carbon/icons-react";
 
-// interface IParentList {}
+interface IParentList {
+  children: React.ReactNode;
+  settingIcon?: boolean;
+  labelList: string;
+}
 
-export const ParentList = () => {
+export const ParentList = ({
+  children,
+  settingIcon,
+  labelList,
+}: IParentList) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleParentList = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="parent-list">
-      <div className="title">Jagung Pipil</div>
-      <div className="wrapper">
-        <div className="icon">
-          <Settings24 />
-        </div>
-        <div className="icon">
-          <ChevronDown24 />
+      <div
+        className={`dropdown-parent ${isOpen ? "focus" : ""}`}
+        onClick={toggleParentList}
+      >
+        <div className="title">{labelList}</div>
+        <div className="wrapper">
+          {settingIcon ? (
+            <div className="icon">
+              <Settings24 />
+            </div>
+          ) : null}
+          <div className="icon">
+            <ChevronDown24 />
+          </div>
         </div>
       </div>
+      <ul className="menu">{children}</ul>
     </div>
   );
 };
